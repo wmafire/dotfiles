1829d1251962034f5549acda415b602332a3b908
meetcw <meetcw@outlook.com>
2019-11-27 00:46:09 +0800

    add some shell scripts

---
+++ script/nvidia_to_nouveau.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# nvidia -> nouveau
+
+/usr/bin/sudo /bin/sed -i 's/#options nouveau modeset=1/options nouveau modeset=1/' /etc/modprobe.d/modprobe.conf
+/usr/bin/sudo /bin/sed -i 's/#MODULES="nouveau"/MODULES="nouveau"/' /etc/mkinitcpio.conf
+
+/usr/bin/sudo /usr/bin/pacman -Rdds --noconfirm nvidia-dkms
+/usr/bin/sudo /usr/bin/pacman -S --noconfirm mesa xf86-video-nouveau
+
+/usr/bin/sudo /sbin/mkinitcpio -p linux
+/usr/bin/sudo /sbin/mkinitcpio -p linux-lts
